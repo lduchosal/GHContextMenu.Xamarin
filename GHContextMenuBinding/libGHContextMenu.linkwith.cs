407ba329ba6ae6aea5c89007cf012a5e25ef8c8b
lduchosal <lduchosal>
2016-01-09 12:40:41 +0100

    arm64 deployment

--- GHContextMenuBinding/libGHContextMenu.linkwith.cs
+++ GHContextMenuBinding/libGHContextMenu.linkwith.cs
@@ -1,3 +1,10 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("libGHContextMenu.a", SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libGHContextMenu.a", 
+	LinkTarget.ArmV7 
+	| LinkTarget.ArmV7s 
+	| LinkTarget.Arm64 
+	| LinkTarget.Simulator64 
+	| LinkTarget.Simulator, 
+	Frameworks = "CoreData, SystemConfiguration, MobileCoreServices, Foundation, CoreLocation, Security, QuartzCore", 
+	SmartLink = true, ForceLoad = true)]
